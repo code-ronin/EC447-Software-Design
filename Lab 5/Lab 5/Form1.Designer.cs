acfc72677119027bab8c94c6e8b3aa3ee4390ca0
Raymond Li <li.raymond9@gmail.com>
2014-10-30 00:48:31 -0400

    Lab 5
    This is a simple drawing program in C#, where you can draw text, lines,
    rectangles, and eclipse

---
+++ Lab 5/Lab 5/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Lab_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PenWidth = new System.Windows.Forms.ListBox();
+            this.FillColor = new System.Windows.Forms.ListBox();
+            this.PenColor = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.text = new System.Windows.Forms.TextBox();
+            this.lineButton = new System.Windows.Forms.RadioButton();
+            this.rectangleButton = new System.Windows.Forms.RadioButton();
+            this.ellipseButton = new System.Windows.Forms.RadioButton();
+            this.textButton = new System.Windows.Forms.RadioButton();
+            this.fill_box = new System.Windows.Forms.CheckBox();
+            this.outline_box = new System.Windows.Forms.CheckBox();
+            this.drawPanel = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(677, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.undoToolStripMenuItem.Text = "Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.PenWidth);
+            this.panel1.Controls.Add(this.FillColor);
+            this.panel1.Controls.Add(this.PenColor);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.fill_box);
+            this.panel1.Controls.Add(this.outline_box);
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(677, 233);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(437, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Pen Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Fill Color";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Pen Color";
+            // 
+            // PenWidth
+            // 
+            this.PenWidth.FormattingEnabled = true;
+            this.PenWidth.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.PenWidth.Location = new System.Drawing.Point(440, 27);
+            this.PenWidth.Name = "PenWidth";
+            this.PenWidth.Size = new System.Drawing.Size(120, 134);
+            this.PenWidth.TabIndex = 9;
+            // 
+            // FillColor
+            // 
+            this.FillColor.FormattingEnabled = true;
+            this.FillColor.Items.AddRange(new object[] {
+            "White",
+            "Black",
+            "Red",
+            "Blue",
+            "Green"});
+            this.FillColor.Location = new System.Drawing.Point(344, 27);
+            this.FillColor.Name = "FillColor";
+            this.FillColor.Size = new System.Drawing.Size(70, 69);
+            this.FillColor.TabIndex = 8;
+            // 
+            // PenColor
+            // 
+            this.PenColor.FormattingEnabled = true;
+            this.PenColor.Items.AddRange(new object[] {
+            "Black",
+            "Red",
+            "Blue",
+            "Green"});
+            this.PenColor.Location = new System.Drawing.Point(247, 27);
+            this.PenColor.Name = "PenColor";
+            this.PenColor.Size = new System.Drawing.Size(70, 69);
+            this.PenColor.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.text);
+            this.groupBox1.Controls.Add(this.lineButton);
+            this.groupBox1.Controls.Add(this.rectangleButton);
+            this.groupBox1.Controls.Add(this.ellipseButton);
+            this.groupBox1.Controls.Add(this.textButton);
+            this.groupBox1.Location = new System.Drawing.Point(22, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 198);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Draw";
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(32, 119);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.text.Size = new System.Drawing.Size(174, 79);
+            this.text.TabIndex = 6;
+            this.text.WordWrap = false;
+            // 
+            // lineButton
+            // 
+            this.lineButton.AutoSize = true;
+            this.lineButton.Location = new System.Drawing.Point(32, 26);
+            this.lineButton.Name = "lineButton";
+            this.lineButton.Size = new System.Drawing.Size(45, 17);
+            this.lineButton.TabIndex = 2;
+            this.lineButton.TabStop = true;
+            this.lineButton.Text = "Line";
+            this.lineButton.UseVisualStyleBackColor = true;
+            // 
+            // rectangleButton
+            // 
+            this.rectangleButton.AutoSize = true;
+            this.rectangleButton.Location = new System.Drawing.Point(32, 49);
+            this.rectangleButton.Name = "rectangleButton";
+            this.rectangleButton.Size = new System.Drawing.Size(74, 17);
+            this.rectangleButton.TabIndex = 3;
+            this.rectangleButton.TabStop = true;
+            this.rectangleButton.Text = "Rectangle";
+            this.rectangleButton.UseVisualStyleBackColor = true;
+            // 
+            // ellipseButton
+            // 
+            this.ellipseButton.AutoSize = true;
+            this.ellipseButton.Location = new System.Drawing.Point(32, 72);
+            this.ellipseButton.Name = "ellipseButton";
+            this.ellipseButton.Size = new System.Drawing.Size(55, 17);
+            this.ellipseButton.TabIndex = 4;
+            this.ellipseButton.TabStop = true;
+            this.ellipseButton.Text = "Ellipse";
+            this.ellipseButton.UseVisualStyleBackColor = true;
+            // 
+            // textButton
+            // 
+            this.textButton.AutoSize = true;
+            this.textButton.Location = new System.Drawing.Point(32, 95);
+            this.textButton.Name = "textButton";
+            this.textButton.Size = new System.Drawing.Size(46, 17);
+            this.textButton.TabIndex = 5;
+            this.textButton.TabStop = true;
+            this.textButton.Text = "Text";
+            this.textButton.UseVisualStyleBackColor = true;
+            // 
+            // fill_box
+            // 
+            this.fill_box.AutoSize = true;
+            this.fill_box.Location = new System.Drawing.Point(258, 134);
+            this.fill_box.Name = "fill_box";
+            this.fill_box.Size = new System.Drawing.Size(38, 17);
+            this.fill_box.TabIndex = 0;
+            this.fill_box.Text = "Fill";
+            this.fill_box.UseVisualStyleBackColor = true;
+            // 
+            // outline_box
+            // 
+            this.outline_box.AutoSize = true;
+            this.outline_box.Location = new System.Drawing.Point(258, 157);
+            this.outline_box.Name = "outline_box";
+            this.outline_box.Size = new System.Drawing.Size(59, 17);
+            this.outline_box.TabIndex = 0;
+            this.outline_box.Text = "Outline";
+            this.outline_box.UseVisualStyleBackColor = true;
+            // 
+            // drawPanel
+            // 
+            this.drawPanel.Location = new System.Drawing.Point(0, 257);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(677, 193);
+            this.drawPanel.TabIndex = 0;
+            this.drawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawPanel_Paint);
+            this.drawPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 450);
+            this.Controls.Add(this.drawPanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lab 5 - Raymond Li";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel drawPanel;
+        private System.Windows.Forms.CheckBox outline_box;
+        private System.Windows.Forms.CheckBox fill_box;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton lineButton;
+        private System.Windows.Forms.RadioButton rectangleButton;
+        private System.Windows.Forms.RadioButton ellipseButton;
+        private System.Windows.Forms.RadioButton textButton;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.ListBox PenColor;
+        private System.Windows.Forms.ListBox FillColor;
+        private System.Windows.Forms.ListBox PenWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab 5/Lab 5/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_5
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList objects = new ArrayList();    //Store graphic objects drawn in this array
+        private bool mouseFirst = true;                 //For keeping track of the first click
+        private Point startPoint;
+        private Point endPoint;
+        public Form1()
+        {
+            InitializeComponent();
+            //Set initial index to start at the top of the columns
+            this.PenColor.SelectedIndex = 0;
+            this.FillColor.SelectedIndex = 0;
+            this.PenWidth.SelectedIndex = 0;
+        }
+        //Clear the objects drawn on the draw_panel
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.objects.Clear();
+            this.drawPanel.Invalidate();
+            base.Update();
+        }
+        //Undo least object created
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.objects.Count > 0) //Check to make sure count is greater than 0
+            {
+                this.objects.RemoveAt(this.objects.Count - 1);
+            }
+            this.drawPanel.Invalidate();
+            base.Update();
+        }
+        //Exit the program
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            base.Close();
+        }
+        //The drawPanel is where everything is drawn
+            //drawPanel paint handler
+        private void drawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            //When any of the objects are selected (line, rectangle, ellipse, text) draw it
+            foreach (MyObject myObject in this.objects)
+            {
+                myObject.draw(graphics);
+            }
+        }
+            //drawPanel mouse handler
+        private void drawPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Document the first click in the drawPanel
+            if (this.mouseFirst)
+            {
+                this.startPoint = e.Location;
+                this.mouseFirst = false;
+                return;
+            }
+            //Reset the mouseFirst upon the second click
+            this.endPoint = e.Location;
+            this.mouseFirst = true;
+            Brush brush = null;     //Pen Color
+            Brush brush2 = null;    //Fill Color
+            Pen pen = null;         //Pen
+            //Set the Pen Color
+            switch (this.PenColor.SelectedIndex)
+            {
+                case 0:
+                    brush = Brushes.Black;
+                    break;
+                case 1:
+                    brush = Brushes.Red;
+                    break;
+                case 2:
+                    brush = Brushes.Blue;
+                    break;
+                case 3:
+                    brush = Brushes.Green;
+                    break;
+            }
+            //If either Line object or Outline box is checked, you need to make the pen the correct color and width
+            if (this.outline_box.Checked || this.lineButton.Checked)
+            {
+                pen = new Pen(brush, (float)int.Parse((string)this.PenWidth.SelectedItem));
+            }
+            //Set Fill Color, only if filBox is checked
+            if (this.fill_box.Checked)
+            {
+                switch (this.FillColor.SelectedIndex)
+                {
+                    case 0:
+                        brush2 = Brushes.White;
+                        break;
+                    case 1:
+                        brush2 = Brushes.Black;
+                        break;
+                    case 2:
+                        brush2 = Brushes.Red;
+                        break;
+                    case 3:
+                        brush2 = Brushes.Blue;
+                        break;
+                    case 4:
+                        brush2 = Brushes.Green;
+                        break;
+                }
+            }
+            //If line box is check, you will draw a Line
+            if (this.lineButton.Checked)
+            {
+                this.objects.Add(new MyLine(this.startPoint, this.endPoint, pen));
+            }
+            //If Fill is checked and the pen is working (cannot draw with a broken pen)
+            if (brush2 != null || pen != null)
+            {
+                //Draw a Rectangle
+                if (this.rectangleButton.Checked)
+                {
+                    this.objects.Add(new MyRectangle(this.startPoint, this.endPoint, pen, brush2));
+                }
+                //Draw an Ellipse
+                if (this.ellipseButton.Checked)
+                {
+                    this.objects.Add(new MyEllipse(this.startPoint, this.endPoint, pen, brush2));
+                }
+            }
+            //If Text box is checked and it's not empty, draw the text in the box
+            if (this.textButton.Checked && this.text.Text != "")
+            {
+                this.objects.Add(new MyText(this.text.Text, this.Font, brush, this.startPoint, this.endPoint));
+            }
+            //Update the drawPanel
+            this.drawPanel.Invalidate();
+            base.Update();
+        }
+    }
+    //Create the base class MyObject
+    //This base class doesn't do anything, it's just for easier refernece in drawPanel_Paint
+    public class MyObject
+    {
+        public virtual void draw(Graphics g)
+        {
+        }
+    }
+    //Class for drawing Line
+    public class MyLine : MyObject
+    {
+        private Point start;
+        private Point end;
+        private Pen pen;
+        public MyLine(Point start, Point end, Pen pen)
+        {
+            this.start = start;
+            this.end = end;
+            this.pen = pen;
+        }
+        public override void draw(Graphics g)
+        {
+            g.DrawLine(this.pen, this.start, this.end);
+        }
+    }
+    //Class for drawing Rectangle
+    public class MyRectangle : MyObject
+    {
+        private Point start;
+        private Point end;
+        private Pen pen;
+        private Brush brush;
+        public MyRectangle(Point start, Point end, Pen pen, Brush brush)
+        {
+            this.start = start;
+            this.end = end;
+            this.pen = pen;
+            this.brush = brush;
+        }
+        public override void draw(Graphics g)
+        {
+            int width = Math.Abs(this.end.X - this.start.X);
+            int height = Math.Abs(this.end.Y - this.start.Y);
+            int x = Math.Min(this.start.X, this.end.X);
+            int y = Math.Min(this.start.Y, this.end.Y);
+            if (this.brush != null)
+            {
+                g.FillRectangle(this.brush, x, y, width, height);
+            }
+            if (this.pen != null)
+            {
+                g.DrawRectangle(this.pen, x, y, width, height);
+            }
+        }
+    }
+    //Class for drawing Ellipse
+    public class MyEllipse : MyObject
+    {
+        private Point start;
+        private Point end;
+        private Pen pen;
+        private Brush brush;
+        public MyEllipse(Point start, Point end, Pen pen, Brush brush)
+        {
+            this.start = start;
+            this.end = end;
+            this.pen = pen;
+            this.brush = brush;
+        }
+        public override void draw(Graphics g)
+        {
+            int width = Math.Abs(this.end.X - this.start.X);
+            int height = Math.Abs(this.end.Y - this.start.Y);
+            int x = Math.Min(this.start.X, this.end.X);
+            int y = Math.Min(this.start.Y, this.end.Y);
+            if (this.brush != null)
+            {
+                g.FillEllipse(this.brush, x, y, width, height);
+            }
+            if (this.pen != null)
+            {
+                g.DrawEllipse(this.pen, x, y, width, height);
+            }
+        }
+    }
+    //Class for drawing Text
+    public class MyText : MyObject
+    {
+        private Point start;
+        private Point end;
+        private Brush brush;
+        private string s;
+        private Font font;
+        public MyText(string s, Font font, Brush brush, Point start, Point end)
+        {
+            this.start = start;
+            this.end = end;
+            this.brush = brush;
+            this.font = font;
+            this.s = s;
+        }
+        public override void draw(Graphics g)
+        {
+            int width = Math.Abs(this.end.X - this.start.X);
+            int height = Math.Abs(this.end.Y - this.start.Y);
+            int x = Math.Min(this.start.X, this.end.X);
+            int y = Math.Min(this.start.Y, this.end.Y);
+            g.DrawString(this.s, this.font, this.brush, new Rectangle(x, y, width, height));
+        }
+    }
+
+}
+
